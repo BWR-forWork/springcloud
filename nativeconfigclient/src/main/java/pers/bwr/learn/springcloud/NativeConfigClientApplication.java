4bcf40d76aa18133a1517c20850a0db1e8628d4e
黑色的白兔子 <0500aa@protonmail.com>
2021-01-06 22:12:40 +0800

    本地配置中心

---
+++ nativeconfigclient/src/main/java/pers/bwr/learn/springcloud/NativeConfigClientApplication.java
@@ -0,0 +1,11 @@
+package pers.bwr.learn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NativeConfigClientApplication.class,args);
+    }
+}
